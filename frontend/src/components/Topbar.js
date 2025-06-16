057da93236cac4de49826440f2868bf3d9fad60e
Nuriddin Sharipov <sharipov.n@eeho.uz>
2025-06-16 12:37:58 +0500

    ready prod 1.5

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -34,7 +34,7 @@ export default function Topbar({ sidebarVisible, toggleSidebar,searchQuery, setS
   }, [fetchUnreadCount]);
 
   return (
-    <div className="d-flex justify-content-between align-items-center px-3 py-2 border" style={{ color: 'white',minHeight:'5vh' }}>
+    <div className="d-flex justify-content-between align-items-center px-3 py-2 border-0" style={{ color: 'white',minHeight:'5vh' }}>
       
       {/* Left: Toggle + Search */}
       <div className="d-flex align-items-center gap-3">
